2ee218d3ba4b541fdf609ef084716279e3eac7df
meshal <imeshalfm@gmail.com>
2024-10-15 12:02:34 +0300

    complete task 3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,16 @@ import Image from 'next/image';
 import storeImg from '../assets/store-front.jpg';
 import { products } from './products';
 
+const productsList = products.map((product) => (
+  <div key={product.name} className="bg-zinc-900 rounded-3xl p-4 flex items-center shadow-lg w-[300px] h-[150px] gap-4 border-2 border-zinc-200/[.1]">
+    <Image src={product.image} alt={product.name} width={100} height={100} />
+    <div className="flex flex-col gap-1">
+      <h3 className="text-zinc-200 text-xl font-bold">{product.name}</h3>
+      <p className="text-zinc-400 text-lg font-semibold">${product.price}</p>
+    </div>
+  </div>
+));
+
 export default function Home() {
   return (
     <div className="bg-zinc-950 flex justify-center flex-col items-center pb-24 gap-6 min-h-screen container mx-auto">
@@ -20,20 +30,7 @@ export default function Home() {
       <div className="mt-20 mb-10 font-black text-4xl" id="products">
         Products
       </div>
-      <div className="flex gap-8 flex-wrap justify-center items-center mx-20">
-        {products.map((product) => (
-          <div
-            key={product.name}
-            className="bg-zinc-900 rounded-3xl p-4 flex items-center shadow-lg w-[300px] h-[150px] gap-4 border-2 border-zinc-200/[.1]"
-          >
-            <Image src={product.image} alt={product.name} width={100} height={100} />
-            <div className="flex flex-col gap-1">
-              <h3 className="text-zinc-200 text-xl font-bold">{product.name}</h3>
-              <p className="text-zinc-400 text-lg font-semibold">${product.price}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className="flex gap-8 flex-wrap justify-center items-center mx-20">{productsList}</div>
     </div>
   );
 }
